3d1e056e384a81b9494778c2a84e5b1b884701c1
tareef mando <tareef@fastmail.com>
2018-11-29 13:39:36 +0300

    fix the chart due to api changes

--- standalone/chart/chart.js
+++ standalone/chart/chart.js
@@ -1,26 +1,30 @@
-var units = [];
-var data= [];
+let units = [];
+let data = [];
 
-d3.json("https://api.syrianarchive.org/incidents").then(function(d) {
+d3.json('https://api.syrianarchive.org/incidents').then((d) => {
+  const urlString = window.location.href;
+  const url = new URL(urlString);
+  const collectionName = url.searchParams.get('collection');
   units = d.units;
-  units = units.filter((obj) => obj.incident_date !== '');
+  units = _.filter(units, u => _.includes(u.clusters.collections,collectionName));
+  units = _.filter(units, u => u.annotations.incident_date !== undefined);
   drawChart();
 });
 
-  const drawChart = () => {
-  // settting up the data in this.props.units to the chart.
+const drawChart = () => {
+  // setting up the data in this.props.units to the chart.
   units.forEach((d) => {
-    const formatedDate = moment(d.incident_date).format('YYYY-MM-DD');
+    const formatedDate = moment(d.annotations.incident_date).format('YYYY-MM-DD');
     if (formatedDate === 'Invalid date') {
-      d.incident_date = moment(d.incident_date, 'DD-MM-YYYY').format('YYYY-MM-DD'); // eslint-disable-line
+      d.annotations.incident_date = moment(d.annotations.incident_date, 'DD-MM-YYYY').format('YYYY-MM-DD'); // eslint-disable-line
     } else {
-      d.incident_date = formatedDate; // eslint-disable-line
+      d.annotations.incident_date = formatedDate; // eslint-disable-line
     }
   });
 
   units = _.groupBy(units, (value) =>
-    value.incident_date
-  );
+                    value.annotations.incident_date
+                   );
 
   const formatDate = d3.timeParse('%Y-%m-%d');
 
@@ -31,8 +35,8 @@ d3.json("https://api.syrianarchive.org/incidents").then(function(d) {
       incidents: Object.values(units[d])
     });
   }
-  // add dates which don't have incidents
 
+  // add dates which don't have incidents
   const dates = [];
   const findDate = data.map((d) => moment(d.date, 'YYYY-MM-DD'));
   const startDate = moment.min(findDate);
@@ -49,21 +53,21 @@ d3.json("https://api.syrianarchive.org/incidents").then(function(d) {
   const height = 200 - margin.top - margin.bottom;
 
   const x = d3.scaleTime()
-      .range([0, width]);
+        .range([0, width]);
 
   const y = d3.scaleLinear()
         .range([height, 0]);
 
   const line = d3.line()
-  .x((d) => x(d.date))
-  .y((d) => y(d.counts))
-  .curve(d3.curveStepAfter);
+        .x((d) => x(d.date))
+        .y((d) => y(d.counts))
+        .curve(d3.curveStepAfter);
 
   const xAxis = d3.axisBottom(x).ticks(d3.timeYear.every(1)).tickFormat(d3.timeFormat('%Y'));
 
   const yAxis = d3.axisLeft(y).ticks(4);
   const chart = d3.select('.chart')
-      .attr('width', width + margin.left + margin.right)
+        .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom);
 
   const g = chart.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')'); // eslint-disable-line
@@ -94,4 +98,4 @@ d3.json("https://api.syrianarchive.org/incidents").then(function(d) {
     .attr('stroke-linecap', 'round')
     .attr('stroke-width', 3)
     .attr('d', line(newData));
-}
+};
